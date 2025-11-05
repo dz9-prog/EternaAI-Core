c2d6c5f78e87b5406429b73659e02e5a089d8348
dz9-prog <murilo19cb@gmail.com>
2025-11-05 03:02:07 -0300

    Delete scripts/scripts/scripts/payment.js

--- scripts/scripts/scripts/payment.js
+++
@@ -1,9 +0,0 @@
-import Stripe from "stripe";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
-export async function checkPayments() {
-  console.log("💰 Verificando pagamentos...");
-  // Aqui você pode listar clientes, assinaturas e automatizar ações
-  const customers = await stripe.customers.list({ limit: 5 });
-  customers.data.forEach(c => console.log("Cliente ativo:", c.email));
-}
